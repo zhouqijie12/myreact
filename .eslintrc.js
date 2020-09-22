ddb110acbde7c4cd95d6fdbbb3b1720cea1e314f
leonzhou <leon@leon.local>
2020-09-22 19:31:03 +0800

    修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true
     },
     "extends": [
         "eslint:recommended",
@@ -18,7 +18,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "semi": ["error", "never"],
+        "semi": ["error", "always"],
         "quotes": ["error", "single"]
     }
 };
